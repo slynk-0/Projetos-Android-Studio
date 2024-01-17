ba7ad6265f2d5a4ab23557b2147bc922a493499e
slynk-0 <brenobertunes05@gmail.com>
2024-01-17 13:54:51 -0300

    Foram criados: Atributos, Métodos de acesso e Objetos.

---
+++ appListaVIP/app/src/main/java/com/example/applistavip/model/Pessoa.java
@@ -0,0 +1,54 @@
+package com.example.applistavip.model;
+
+public class Pessoa {
+
+    public Pessoa() {
+    } //Esse é o construtor da classe, do qual permite que a classe seja instanciada
+    //por exemplo na MainActivity.
+
+
+    //Definindo os atributos:
+    private String primeiroNome;
+    private String sobrenome;
+    private String cursoDesejado;
+    private String telefone;
+
+
+/*
+    Utilizando dos métodos de acesso Getter and Setter, podemos fazer os atributos ficarem
+    acessíveis para outra classe. Basta clicar com o botão direito: Generate > Getter and Setter
+    Selecionar todas os atributos e dar enter.
+*/
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
--- appListaVIP/app/src/main/java/com/example/applistavip/view/MainActivity.java
+++ appListaVIP/app/src/main/java/com/example/applistavip/view/MainActivity.java
@@ -5,12 +5,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.applistavip.R;
+import com.example.applistavip.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
+
+    Pessoa pessoa; //Instanciando a Classe Pessoa através do Construtor e definindo o objeto pessoa.
+    Pessoa outraPessoa; //Definindo o objeto outraPessoa.
+
+    Pessoa dados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa(); //Criando um objeto para a classe Pessoa.
+        outraPessoa = new Pessoa();
+        String dados; //Definindo uma String para ser usada para captar todos os dados de um objeto.
+        String dadosOutraPessoa;
+
+        //Definindo valores para os atributos criados na classe Pessoa:
+
+        pessoa.setPrimeiroNome("Breno");
+        pessoa.setSobrenome("Sodré Bertunes");
+        pessoa.setCursoDesejado("Desenvolvimento Android");
+        pessoa.setTelefone("4002-8922");
+
+        outraPessoa.setPrimeiroNome("John");
+        outraPessoa.setSobrenome("Doe");
+        outraPessoa.setCursoDesejado("Desenvolvimento em C#");
+        outraPessoa.setTelefone("2934-2445");
+
+        dados = "Primeiro nome: ";
+                dados += pessoa.getPrimeiroNome();
+                dados += "Sobrenome: ";
+                dados += pessoa.getSobrenome();
+                dados += "Curso desejado: ";
+                dados += pessoa.getCursoDesejado();
+                dados += "Telefone: ";
+                dados += pessoa.getTelefone();
+
+        dadosOutraPessoa = "Primeiro nome: ";
+                dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+                dadosOutraPessoa += "Sobrenome: ";
+                dadosOutraPessoa += outraPessoa.getSobrenome();
+                dadosOutraPessoa += "Curso desejado: ";
+                dadosOutraPessoa += outraPessoa.getCursoDesejado();
+                dadosOutraPessoa += "Telefone: ";
+                dadosOutraPessoa += outraPessoa.getTelefone();
+
     }
 }
