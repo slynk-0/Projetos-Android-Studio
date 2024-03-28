49a43cdb06714031b7754fad0123ad24bb359066
slynk-0 <brenobertunes05@gmail.com>
2024-03-28 11:21:04 -0300

    Limpeza do código.

--- appListaVIP/app/src/main/java/com/example/applistavip/model/Pessoa.java
+++ appListaVIP/app/src/main/java/com/example/applistavip/model/Pessoa.java
@@ -1,18 +1,15 @@
 package com.example.applistavip.model;
 
 public class Pessoa {
-
     public Pessoa() {
     } //Esse é o construtor da classe, do qual permite que a classe seja instanciada
     //por exemplo na MainActivity.
-
 
     //Definindo os atributos:
     private String primeiroNome;
     private String sobrenome;
     private String cursoDesejado;
     private String telefone;
-
 
 /*
     Utilizando dos métodos de acesso Getter and Setter, podemos fazer os atributos ficarem
@@ -23,27 +20,21 @@ public class Pessoa {
     public String getPrimeiroNome() {
         return primeiroNome;
     }
-
     public void setPrimeiroNome(String primeiroNome) {
         this.primeiroNome = primeiroNome;
     }
-
     public String getSobrenome() {
         return sobrenome;
     }
-
     public void setSobrenome(String sobrenome) {
         this.sobrenome = sobrenome;
     }
-
     public String getCursoDesejado() {
         return cursoDesejado;
     }
-
     public void setCursoDesejado(String cursoDesejado) {
         this.cursoDesejado = cursoDesejado;
     }
-
     public String getTelefone() {
         return telefone;
     }
